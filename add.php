29955ac86a9c91744682f4e76f4e5178fbd1f1a8
MAARTEN-LAPTOP\Maarten <maartenhulsman95@gmail.com>
2016-08-08 14:18:35 +0200

    test

--- add.php
+++ add.php
@@ -11,7 +11,7 @@ $artist = mysqli_real_escape_string($conn, $_POST["artist"]);
 $title = mysqli_real_escape_string($conn, $_POST["title"]);
 $year = mysqli_real_escape_string($conn, $_POST["year"]);
 
-$sql = "INSERT INTO (album, artist, title, year)
+$sql = "INSERT INTO album (artist, title, year)
         VALUES ('$artist', '$title', '$year')";
 $result = $conn->query($sql);
 $conn->close();
